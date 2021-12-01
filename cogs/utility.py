3b75e2e51a5997bb8a6aa6248b42d22ea9beca07
TheHa3kerDevs <79122807+Ha3kerDevs@users.noreply.github.com>
2021-12-01 13:32:07 +0800

    asdasads

--- cogs/utility.py
+++ cogs/utility.py
@@ -74,9 +74,9 @@ class Utility(commands.Cog, name="Utility"):
     aliases=['td']
   )
   async def timedif(
-    self, ctx, 
-    id1: int = commands.Option(description="PLEASE PRESS TAB WHEN YOU FINISHED PASTING ID1"), 
-    id2: int = commands.Option(description="PLEASE PRESS TAB WHEN YOU FINISHED PASTING ID2")
+      self, ctx, 
+      id1: commands.Option(description="PLEASE PRESS TAB WHEN YOU FINISHED PASTING ID1"), 
+      id2: commands.Option(description="PLEASE PRESS TAB WHEN YOU FINISHED PASTING ID2")
     ):
       try:
         id1 = int(id1)
