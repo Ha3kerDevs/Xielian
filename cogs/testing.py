cf5624d6f03e1cebb54136a06842d6f2225eaa5a
TheHa3kerDevs <79122807+Ha3kerDevs@users.noreply.github.com>
2022-01-14 10:02:00 +0800

    minor changes

--- cogs/testing.py
+++ cogs/testing.py
@@ -1,7 +1,7 @@
 import discord
 import datetime
 import asyncio
-from cogs.utils import checks, config
+from cogs.utils import checks, config, utils
 from discord.ext import commands
 from typing import Optional
 from discord.utils import get
@@ -18,58 +18,50 @@ class TestingQ(commands.Cog, command_attrs=dict(hidden=True), name="Testing"):
     self.bot = bot
   
 
-  @commands.has_any_role(
-    793679885285326890,
-    822727647087165461
-  )
+  @commands.is_owner()
   @commands.guild_only()
   @commands.command(hidden=True)
   async def slashdelete(self, ctx, gid: int):
-    
-    if ctx.author.id == 341837496763678731:
-      await self.bot.http.bulk_upsert_guild_commands(self.bot.application_id, gid, [])
-      return
-    else:
-      return
-  
+    await self.bot.http.bulk_upsert_guild_commands(self.bot.application_id, gid, [])
 
-  @commands.has_any_role(
-    793679885285326890,
-    822727647087165461
-  )
+  
+  @commands.is_owner()
   @commands.guild_only()
   @commands.command(hidden=True)
   async def botmsg(self, ctx, *, msg):
-    
-    if ctx.author.id == 341837496763678731:
-      if ctx.message.reference is not None:
-        message = await ctx.channel.fetch_message(ctx.message.reference.message_id)
-        await message.reply(f"{msg}", allowed_mentions=discord.AllowedMentions().none())
-        await ctx.message.delete()
-        return
-      else:
-        await ctx.send(f"{msg}")
-        await ctx.message.delete()
-        return
+
+    if ctx.message.reference is not None:
+      message = await ctx.channel.fetch_message(ctx.message.reference.message_id)
+      await message.reply(f"{msg}", allowed_mentions=discord.AllowedMentions().none())
+      await ctx.message.delete()
+      return
     else:
+      await ctx.send(f"{msg}")
+      await ctx.message.delete()
       return
 
-  @commands.has_any_role(
-    793679885285326890,
-    822727647087165461
-  )
+  @commands.is_owner()
   @commands.guild_only()
   @commands.command(hidden=True)
   async def timetest(self, ctx, time: int):
     
-    if ctx.author.id == 341837496763678731:
-      await ctx.send(f"Text start done. Timer: `{time}`")
-      await asyncio.sleep(time)
-      await ctx.send(f"Text finish done. Timer: `{time}`")
-      return
-    else:
-      return
+    await ctx.send(f"Text start done. Timer: `{time}`")
+    await asyncio.sleep(time)
+    await ctx.send(f"Text finish done. Timer: `{time}`")
 
+  @commands.is_owner()
+  @commands.guild_only()
+  @commands.command(hidden=True)
+  async def outuserinfo(self, ctx, *, user: Union[discord.Member, discord.User] = None):
+    user = user or ctx.author
+    e = discord.Embed(title="User info", color=0xf8c7c7)
+
+    e.add_field(name='ID', value=user.id, inline=False)
+    e.add_field(name='Joined', value=f"{utils.timestamp(user.joined_at, 'f')}", inline=False)
+    e.add_field(name='Created', value=f"{utils.timestamp(user.created_at, 'f')}", inline=False)
+    e.set_thumbnail(url=user.display_avatar.url)
+
+    await ctx.send(embed=e)
 
 def setup(bot: StellaricBot):
   bot.add_cog(TestingQ(bot))
